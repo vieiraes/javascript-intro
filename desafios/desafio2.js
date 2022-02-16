5a0d3556273da5f9520fc743b45a0eae933d2694
Bruno Vieira <vieira.es@gmail.com>
2022-02-16 00:13:20 -0300

    added more desafio

--- desafios/desafio2.js
+++ desafios/desafio2.js
@@ -1,30 +1,23 @@
 //DESAFIO2: calcule a soma de um array de numeros inteiros
 
 function desafio2(numeros = []) {
-
-    // var contador = 0
-    // var posicao = numeros.length
+    
     var array = numeros;
-    var soma = 0;
     var contador = 0;
+    var soma = 0;
+    
     var numeroItemsArray = array.length;
-
-
-    //console.log(`${numeroItemsArray}`);
     
     for (contador; contador < numeroItemsArray; contador++) {
         
-        // var resultado; 
-        var posicao = array[contador];
-        //var resultado = posicao;
-        
-        console.log(posicao);
+        var posicao = parseInt(array[contador]);
+        var soma = soma + posicao;
     }
+    console.log(`A soma do array é ${soma}`);
 
 }
 
-desafio2([1, 2, 3, 4, 4, 52, 6, 6]);
+desafio2([1, 1, 1, 4, 4, 52, 6, 6]);
 
 
 
-//DESAFIO AINDA NAO ESTA PRONTO
